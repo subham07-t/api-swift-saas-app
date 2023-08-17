ec14bbfd1494e1da1c12a1c32bc165329c31d8de
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-08-17 23:54:05 +0530

    feat: next-auth setup

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: String(process.env.GITHUB_CLIENT_ID),
+      clientSecret: String(process.env.GITHUB_CLIENT_SECRET),
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+} as NextAuthOptions;
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
