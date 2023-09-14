6370f85e0a3366d000f4c1928f9881547235ce56
Subham07-t <subhamhaldar70@gmail.com>
2023-09-14 11:36:01 +0530

    update nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const authOptions = {
-  adapter: PrismaAdapter(prisma),
+  // adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: String(process.env.GITHUB_CLIENT_ID),
